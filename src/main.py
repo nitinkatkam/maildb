ed14a4fdacbfaa5019a6e157e0a6d13a3b72954e
nitinkatkam <nitinkatkam@gmail.com>
2019-11-16 15:24:02 +0400

    Pass bind_ip and port as command-line parameters.

--- src/main.py
+++ src/main.py
@@ -1,13 +1,20 @@
 import argparse
 from aiosmtpd.controller import Controller
-from src.sqlite_smtp_handler import SqliteHandler
+from sqlite_smtp_handler import SqliteHandler
 
 def main():
   parser = argparse.ArgumentParser()
-#TODO Read the bind IP and port as commandline parameters
-#   parser.add_argument("--bind_ip", help="IP address to listen on")
+  parser.add_argument("--bind_ip", help="IP address to listen on")
+  parser.add_argument("--port", help="TCP port to listen on")
   args = parser.parse_args()
-  Controller(SqliteHandler()).start()
+
+  DEFAULT_HOSTNAME = '0.0.0.0'
+  DEFAULT_PORT = 8025
+
+  hostname = args.bind_ip if args.bind_ip else DEFAULT_HOSTNAME
+  port = args.port if args.port else DEFAULT_PORT
+
+  Controller(SqliteHandler(), hostname=hostname, port=port).start()
 
 if __name__=='__main__':
     main()
